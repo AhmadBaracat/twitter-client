2fb1b410a2a5e8100a7bb0fdcaca99df807e5dc9
Baracat <baracat@aucegypt.edu>
2016-07-05 14:10:20 +0200

    Initial Commit

---
+++ Twitter-Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter-Client
+//
+//  Created by Ahmed Baracat on 7/5/16.
+//  Copyright © 2016 Ahmed Baracat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
