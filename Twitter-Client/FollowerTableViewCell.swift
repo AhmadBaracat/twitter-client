cd896921feeefa5330f5e1bf8b7b68ba4077fdb3
Baracat <baracat@aucegypt.edu>
2016-07-09 16:45:09 +0200

    Initial commit

---
+++ Twitter-Client/FollowerTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FollowerTableViewCell.swift
+//  Twitter-Client
+//
+//  Created by Ahmed Baracat on 7/9/16.
+//  Copyright © 2016 Ahmed Baracat. All rights reserved.
+//
+
+import UIKit
+
+class FollowerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
