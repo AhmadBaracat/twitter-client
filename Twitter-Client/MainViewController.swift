34b1e267753f6f21f4cfa894d7b772fcfbfec132
Baracat <baracat@aucegypt.edu>
2016-07-09 17:01:28 +0200

    Add controller logic to populate the tableView

--- Twitter-Client/MainViewController.swift
+++ Twitter-Client/MainViewController.swift
@@ -7,29 +7,146 @@
 //
 
 import UIKit
+import TwitterKit
+import Argo
+import AlamofireImage
 
-class MainViewController: UIViewController {
 
+class MainViewController: UITableViewController {
+    
+    var followers = [User]()
+    
+    func getFollowers(userID: String, cursor: String)
+    {
+        //Instantiate the Twitter client with the userID
+        let client = TWTRAPIClient(userID: userID)
+        
+        let statusesShowEndpoint = "https://api.twitter.com/1.1/followers/list.json"
+        var clientError : NSError?
+        
+        //Add the cursor which indicates which page to fetch
+        let params = ["cursor": cursor]
+        
+        //Create the request
+        let request = client.URLRequestWithMethod("GET", URL: statusesShowEndpoint, parameters: params, error: &clientError)
+        
+        //Send the request
+        client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+            if connectionError != nil {
+                print("Error: \(connectionError)")
+            }
+            
+            do {
+                if data != nil
+                {
+                    //Convert json String to foundation object
+                    let json: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(data!, options: [])
+                    
+                    if let j: AnyObject = json {
+                        
+                        //Map the foundation object to Response object
+                        let response: Response? = decode(j)
+                        
+                        //Add the users to the followers
+                        for user in (response?.users)!
+                        {
+                            self.followers.append(user)
+                        }
+                        
+                        //Reload the tableView in order to display the newly added followers
+                        self.tableView.reloadData()
+                        
+                        //Get two pages only
+                        if self.followers.count < 30
+                        {
+                            self.getFollowers(userID, cursor: (response?.next_cursor_str)!)
+                        }
+                    }
+                }
+            } catch let jsonError as NSError {
+                print("json error: \(jsonError.localizedDescription)")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //To enable the resizing of the cells
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 300
+        
+        //Get the store of user sessions
+        let store = Twitter.sharedInstance().sessionStore
+        
+        //Get the last user session
+        let lastSession = store.session
+        
+        //Check if user was already logged in
+        if((lastSession()) != nil)
+        {
+            //getFollowers((lastSession()?.userID)!, cursor: "-1")
+            
+            //For testing purposes, not to hit the API limit
+            var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "update", userInfo: nil, repeats: true)
+        }
     }
-
+    
+    //Test method to fill the followers
+    func update() {
+        var user = User(name: "Ahmed Baracat", screen_name: "baracat_wp7", description: "I am a software developer with great passion for design. I have created a number of games and apps for Windows Phone, Android, iOS, watchOS, tvOS :):). I am coo0000000000000llllll", profile_image_url: "https://pbs.twimg.com/profile_images/619282614399180800/IRLGlac6_bigger.jpg")
+        
+        
+        followers.append(user)
+        
+        user = User(name: "Fardia", screen_name: "fifo", description: "", profile_image_url: "https://pbs.twimg.com/profile_images/619282614399180800/IRLGlac6_bigger.jpg")
+        
+        
+        followers.append(user)
+        
+        //Make sure we display the newly added followers
+        tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return followers.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)  as! FollowerTableViewCell
+        
+        //Populate the cell
+        cell.nameLabel?.text = followers[indexPath.row].name
+        cell.screenNameLabel?.text = followers[indexPath.row].screen_name
+        cell.descriptionTextView?.text = followers[indexPath.row].description
+        
+        let URL = NSURL(string: followers[indexPath.row].profile_image_url)!
+        let placeholderImage = UIImage(named: "Placeholder")!
+        cell.profileImageView!.af_setImageWithURL(URL, placeholderImage: placeholderImage)
+        
+        return cell
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
