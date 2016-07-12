5c0bcf1d5b7f48c373a4ccfd261ec1815fd0a5d1
Baracat <baracat@aucegypt.edu>
2016-07-12 22:43:41 +0200

    Add caching requests and images support

--- Twitter-Client/MainViewController.swift
+++ Twitter-Client/MainViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 import TwitterKit
 import Argo
+import Alamofire
 import AlamofireImage
+import CoreData
 
 
 class MainViewController: UITableViewController {
     
     var followers = [User]()
     
-    func getFollowers(userID: String, cursor: String)
+    let imageCache = AutoPurgingImageCache()
+    
+    func parseJson(passedData: NSData?)
+    {
+        do {
+            if let data = passedData
+            {
+                //Convert json String to foundation object
+                let json: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(data, options: [])
+                
+                if let j: AnyObject = json {
+                    
+                    //Map the foundation object to Response object
+                    let response: Response? = decode(j)
+                    
+                    //Add the users to the followers
+                    for user in (response?.users)!
+                    {
+                        self.followers.append(user)
+                    }
+                    
+                    //Reload the tableView in order to display the newly added followers
+                    self.tableView.reloadData()
+                    
+                    //Get two pages only
+                    if self.followers.count < 30
+                    {
+                        //self.getFollowers(session, cursor: (response?.next_cursor_str)!)
+                    }
+                }
+            }
+        } catch let jsonError as NSError {
+            print("json error: \(jsonError.localizedDescription)")
+        }
+        
+    }
+    
+    func getFollowers(session: TWTRAuthSession, cursor: String)
     {
         //Instantiate the Twitter client with the userID
-        let client = TWTRAPIClient(userID: userID)
+        let client = TWTRAPIClient(userID: session.userID)
         
         let statusesShowEndpoint = "https://api.twitter.com/1.1/followers/list.json"
         var clientError : NSError?
@@ -27,44 +66,47 @@ class MainViewController: UITableViewController {
         //Add the cursor which indicates which page to fetch
         let params = ["cursor": cursor]
         
-        //Create the request
-        let request = client.URLRequestWithMethod("GET", URL: statusesShowEndpoint, parameters: params, error: &clientError)
+        //Create the Twitter request to get the required http headers
+        let twitterRequest = client.URLRequestWithMethod("GET", URL: statusesShowEndpoint, parameters: params, error: &clientError)
         
-        //Send the request
-        client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
-            if connectionError != nil {
-                print("Error: \(connectionError)")
-            }
-            
-            do {
-                if data != nil
-                {
-                    //Convert json String to foundation object
-                    let json: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(data!, options: [])
+        //Create the Alamofire reuest
+        let request = Alamofire.Manager.sharedInstance.request(twitterRequest)
+        
+        if(Reachability.isConnectedToNetwork())
+        {
+            //Initiate the request
+            request.responseJSON { (response) in
+                if response.response?.statusCode == 200 {
                     
-                    if let j: AnyObject = json {
-                        
-                        //Map the foundation object to Response object
-                        let response: Response? = decode(j)
-                        
-                        //Add the users to the followers
-                        for user in (response?.users)!
+                    print("Success")
+                    
+                    //Cache the response
+                    let cachedURLResponse = NSCachedURLResponse(response: response.response!, data: (response.data! as NSData), userInfo: nil, storagePolicy: .Allowed)
+                    NSURLCache.sharedURLCache().storeCachedResponse(cachedURLResponse, forRequest: twitterRequest)
+                    
+                    //Parse the response
+                    self.parseJson(response.data)
+                }
+                else {
+                    
+                    print("problem " + String(response))
+                    
+                    if let error = response.result.value as? NSDictionary
+                    {
+                        if let errorMessage = error.objectForKey("message") as? String
                         {
-                            self.followers.append(user)
-                        }
-                        
-                        //Reload the tableView in order to display the newly added followers
-                        self.tableView.reloadData()
-                        
-                        //Get two pages only
-                        if self.followers.count < 30
-                        {
-                            self.getFollowers(userID, cursor: (response?.next_cursor_str)!)
+                            print("error: " + String(error) + "  errorMessage: " + String(errorMessage))
                         }
                     }
                 }
-            } catch let jsonError as NSError {
-                print("json error: \(jsonError.localizedDescription)")
+            }
+        }
+        else
+        {
+            if let response = NSURLCache.sharedURLCache().cachedResponseForRequest(twitterRequest)
+            {
+                //Parse the response
+                self.parseJson(response.data)
             }
         }
     }
@@ -85,10 +127,11 @@ class MainViewController: UITableViewController {
         //Check if user was already logged in
         if((lastSession()) != nil)
         {
-            //getFollowers((lastSession()?.userID)!, cursor: "-1")
+            getFollowers(lastSession()!, cursor: "-1")
+            
             
             //For testing purposes, not to hit the API limit
-            var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "update", userInfo: nil, repeats: true)
+            // var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "update", userInfo: nil, repeats: true)
         }
     }
     
@@ -96,11 +139,9 @@ class MainViewController: UITableViewController {
     func update() {
         var user = User(name: "Ahmed Baracat", screen_name: "baracat_wp7", description: "I am a software developer with great passion for design. I have created a number of games and apps for Windows Phone, Android, iOS, watchOS, tvOS :):). I am coo0000000000000llllll", profile_image_url: "https://pbs.twimg.com/profile_images/619282614399180800/IRLGlac6_bigger.jpg")
         
-        
         followers.append(user)
         
         user = User(name: "Fardia", screen_name: "fifo", description: "", profile_image_url: "https://pbs.twimg.com/profile_images/619282614399180800/IRLGlac6_bigger.jpg")
-        
         
         followers.append(user)
         
@@ -130,13 +171,28 @@ class MainViewController: UITableViewController {
         cell.screenNameLabel?.text = followers[indexPath.row].screen_name
         cell.descriptionTextView?.text = followers[indexPath.row].description
         
-        let URL = NSURL(string: followers[indexPath.row].profile_image_url)!
-        let placeholderImage = UIImage(named: "Placeholder")!
-        cell.profileImageView!.af_setImageWithURL(URL, placeholderImage: placeholderImage)
+        let url = followers[indexPath.row].profile_image_url
+        
+        if(Reachability.isConnectedToNetwork())
+        {
+            Alamofire.request(.GET, url)
+                .responseImage { response in
+                    
+                    if let image = response.result.value {
+                        
+                        cell.profileImageView.image = image
+                        UIImage.saveImageToDisk(NSData(data: UIImageJPEGRepresentation(image, 1.0)!), url: url)
+                    }
+            }
+        }
+        else
+        {
+            cell.profileImageView.image = UIImage.loadImageFromDisk(url)
+        }
         
         return cell
-        
     }
+    
     
     
     /*
