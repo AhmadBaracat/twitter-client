1394b4eabc9aae76e78cb2d27f3894f300663150
Baracat <baracat@aucegypt.edu>
2016-07-14 21:50:55 +0200

    Initial commit

---
+++ Twitter-Client/TweetTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter-Client
+//
+//  Created by Ahmed Baracat on 7/14/16.
+//  Copyright © 2016 Ahmed Baracat. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tweetImageView: UIImageView!
+    
+    @IBOutlet var tweetImageTrailingConstraint: NSLayoutConstraint!
+    override func awakeFromNib() {
+       
+        
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
